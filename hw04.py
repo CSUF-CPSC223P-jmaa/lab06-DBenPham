2078a6963048c77dc9b8b8ff960bda722b3575b7
DBenPham <43255215+DBenPham@users.noreply.github.com>
2023-03-07 18:46:03 -0800

    Add files via upload

--- hw04.py
+++ hw04.py
@@ -128,9 +128,11 @@ def balanced(m):
     True
     """
     "*** YOUR CODE HERE ***"
-    if(total_weight(end(left(m)))*length(left(m))!=total_weight(end(right(m)))*length(right(m))):
-        return False
-    return True
+    if is_planet(m):
+        return True
+    else:
+        return (total_weight(end(left(m)))*length(left(m)))== (total_weight(end(right(m)))*length(right(m))) and balanced(end(left(m))) and balanced(end(right(m)))
+
     
 
 
@@ -165,7 +167,12 @@ def totals_tree(m):
     """
     "*** YOUR CODE HERE ***"
     
-
+    if is_planet(m):
+        return tree(mass(m))
+    else:
+        arm=[totals_tree(end(a(m))) for a in [left,right]]
+        return tree(sum([label(b) for b in arm]),arm)
+    
 def replace_loki_at_leaf(t, lokis_replacement):
     """Returns a new tree where every leaf value equal to "loki" has
     been replaced with lokis_replacement.
@@ -196,7 +203,10 @@ def replace_loki_at_leaf(t, lokis_replacement):
     True
     """
     "*** YOUR CODE HERE ***"
-
+    if(is_leaf(t) and label(t)== 'loki'):
+        return tree(lokis_replacement)
+    else:
+        return tree(label(t),[replace_loki_at_leaf(r,lokis_replacement) for r in branches(t)])
 
 def has_path(t, word):
     """Return whether there is a path in a tree where the entries along the path
@@ -230,7 +240,14 @@ def has_path(t, word):
     """
     assert len(word) > 0, 'no path for empty word.'
     "*** YOUR CODE HERE ***"
-
+    if label(t) != word[0]:
+        return False
+    elif len(word) == 1:
+        return True
+    for b in branches(t):
+        if has_path(b, word[1:]):
+            return True
+    return False
 
 def str_interval(x):
     """Return a string representation of interval x."""
@@ -254,12 +271,12 @@ def interval(a, b):
 def lower_bound(x):
     """Return the lower bound of interval x."""
     "*** YOUR CODE HERE ***"
-
+    return x[0]
 
 def upper_bound(x):
     """Return the upper bound of interval x."""
     "*** YOUR CODE HERE ***"
-
+    return x[1]
 
 def str_interval(x):
     """Return a string representation of interval x."""
@@ -277,24 +294,25 @@ def add_interval(x, y):
 def mul_interval(x, y):
     """Return the interval that contains the product of any value in x and any
     value in y."""
-    p1 = x[0] * y[0]
-    p2 = x[0] * y[1]
-    p3 = x[1] * y[0]
-    p4 = x[1] * y[1]
-    return [min(p1, p2, p3, p4), max(p1, p2, p3, p4)]
+    p1 = lower_bound(x) * lower_bound(y)
+    p2 = lower_bound(x) * upper_bound(y)
+    p3 = upper_bound(x) * lower_bound(y)
+    p4 = upper_bound(x) * upper_bound(y)
+    return interval(min(p1, p2, p3, p4), max(p1, p2, p3, p4))
 
 
 def sub_interval(x, y):
     """Return the interval that contains the difference between any value in x
     and any value in y."""
     "*** YOUR CODE HERE ***"
-
+    return add_interval(x,interval(-upper_bound(y),-lower_bound(y)))
 
 def div_interval(x, y):
     """Return the interval that contains the quotient of any value in x divided by
     any value in y. Division is implemented as the multiplication of x by the
     reciprocal of y."""
     "*** YOUR CODE HERE ***"
+    assert not (lower_bound(y) <= 0 <= upper_bound(y))
     reciprocal_y = interval(1 / upper_bound(y), 1 / lower_bound(y))
     return mul_interval(x, reciprocal_y)
 
@@ -319,8 +337,8 @@ def check_par():
     >>> lower_bound(x) != lower_bound(y) or upper_bound(x) != upper_bound(y)
     True
     """
-    r1 = interval(1, 1)  # Replace this line!
-    r2 = interval(1, 1)  # Replace this line!
+    r1 = interval(1, 2)
+    r2 = interval(3, 4)
     return r1, r2
 
 
